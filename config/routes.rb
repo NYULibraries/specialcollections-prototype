2751b8bd00bfeab0ed9efdd156acafa242fb4349
Brian Kelly <polymonic@gmail.com>
2025-02-28 14:34:09 -0600

    Fix Rubocop issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
-  mount Blacklight::Engine => '/'
+  mount Blacklight::Engine => "/"
   root to: "catalog#index"
   concern :searchable, Blacklight::Routes::Searchable.new
 
-  resource :catalog, only: [], as: 'catalog', path: '/catalog', controller: 'catalog' do
+  resource :catalog, only: [], as: "catalog", path: "/catalog", controller: "catalog" do
     concerns :searchable
   end
   devise_for :users
 
   concern :exportable, Blacklight::Routes::Exportable.new
 
-  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+  resources :solr_documents, only: [ :show ], path: "/catalog", controller: "catalog" do
     concerns :exportable
   end
 
-  resources :bookmarks, only: [:index, :update, :create, :destroy] do
+  resources :bookmarks, only: [ :index, :update, :create, :destroy ] do
     concerns :exportable
 
     collection do
-      delete 'clear'
+      delete "clear"
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
